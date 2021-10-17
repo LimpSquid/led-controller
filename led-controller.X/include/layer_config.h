dccc45098f38eeac1028e4c7cf43508a1fd91035
LimpSquid <git@limpsquid.nl>
2021-10-17 11:27:33 +0200

    Added countdown timer and introduced interlaced row scanning

--- led-controller.X/include/layer_config.h
+++ led-controller.X/include/layer_config.h
@@ -3,7 +3,10 @@
 
 // Notes:
 // - In order to achieve the desired refresh interval, make sure the TLC5940 uses a sufficient GSCLK PWM frequency
+// - When the interlaced config is not used, you'll need to reduce the refresh interval to around 1000us
+// - When the interlaced config is used, the minimum refresh interval is around 1200us
 
-#define LAYER_REFRESH_INTERVAL      1000    // Refresh interval between layers in us
+#define LAYER_REFRESH_INTERVAL      1100    // Refresh interval between layers in us, a bit lower than 1200us to make it smoother
+#define LAYER_INTERLACED                    // Uncomment to use incremental scanning of the rows
 
 #endif	/* LAYER_CONFIG_H */
--- led-controller.X/source/layer.c
+++ led-controller.X/source/layer.c
@@ -7,6 +7,7 @@
 #include <sys.h>
 #include <register.h>
 #include <toolbox.h>
+#include <timer.h>
 #include <stddef.h>
 #include <xc.h>
 #include <string.h>
@@ -24,6 +25,8 @@
 #define LAYER_FRAME_DEPTH           3 // RGB
 #define LAYER_FRAME_BUFFER_SIZE     (LAYER_NUM_OF_LEDS * LAYER_FRAME_DEPTH)
 #define LAYER_LOD_GRAYSCALE_VALUE   4095
+#define LAYER_LOD_SHOW_USER_TIME    1500 // In milliseconds
+#define LAYER_OFFSET(index)         (index * LAYER_NUM_OF_COLS)
 #define LAYER_IO(pin, bank) \
     { \
         .ansel = NULL, \
@@ -77,6 +80,7 @@ enum layer_state
     LAYER_EXEC_LOD_SHOW,
     LAYER_EXEC_LOD_SHOW_WRITE,
     LAYER_EXEC_LOD_SHOW_WRITE_WAIT,
+    LAYER_EXEC_LOD_SHOW_USER_WAIT,
     LAYER_EXEC_LOD_SHOW_DONE,
     LAYER_EXEC_LOD_DONE,
 };
@@ -97,7 +101,68 @@ static void layer_rtask_execute(void);
 static void layer_dma_rtask_execute(void);
 KERN_TTASK(layer, layer_ttask_init, layer_ttask_execute, layer_ttask_configure, KERN_INIT_LATE);
 KERN_SIMPLE_RTASK(layer, layer_rtask_init, layer_rtask_execute);
-KERN_SIMPLE_RTASK(layer_dma, NULL, layer_dma_rtask_execute);
+KERN_SIMPLE_RTASK(layer_dma, NULL, layer_dma_rtask_execute); // Init is done in layer_rtask_init, no need to make a separate init
+
+#ifdef LAYER_INTERLACED
+static const unsigned int layer_offset[LAYER_NUM_OF_ROWS] =
+{
+    LAYER_OFFSET(1), // Row 1
+    LAYER_OFFSET(3), // Row 3
+    LAYER_OFFSET(5), // ...
+    LAYER_OFFSET(7),
+    LAYER_OFFSET(9),
+    LAYER_OFFSET(11),
+    LAYER_OFFSET(13),
+    LAYER_OFFSET(15),
+    LAYER_OFFSET(0), // Row 0
+    LAYER_OFFSET(2), // Row 2
+    LAYER_OFFSET(4), // ...
+    LAYER_OFFSET(6),
+    LAYER_OFFSET(8),
+    LAYER_OFFSET(10),
+    LAYER_OFFSET(12),
+    LAYER_OFFSET(14),
+};
+
+static const struct layer_io layer_io[LAYER_NUM_OF_ROWS] =
+{
+    LAYER_IO(6, D), // Row 1
+    LAYER_IO(4, D), // Row 3
+    LAYER_ANSEL_IO(2, D), // ...
+    LAYER_IO(0, D),
+    LAYER_ANSEL_IO(2, E), 
+    LAYER_IO(0, E),
+    LAYER_IO(10, D),
+    LAYER_IO(8, D),
+    LAYER_IO(7, D), // Row 0
+    LAYER_IO(5, D), // Row 2
+    LAYER_ANSEL_IO(3, D), // ...
+    LAYER_ANSEL_IO(1, D),
+    LAYER_IO(3, E),
+    LAYER_IO(1, E),
+    LAYER_IO(11, D),
+    LAYER_IO(9, D),
+};
+#else
+static const unsigned int layer_offset[LAYER_NUM_OF_ROWS] =
+{
+    LAYER_OFFSET(0), // Row 0
+    LAYER_OFFSET(1), // Row 1
+    LAYER_OFFSET(2), // ...
+    LAYER_OFFSET(3),
+    LAYER_OFFSET(4),
+    LAYER_OFFSET(5),
+    LAYER_OFFSET(6),
+    LAYER_OFFSET(7),
+    LAYER_OFFSET(8),
+    LAYER_OFFSET(9),
+    LAYER_OFFSET(10),
+    LAYER_OFFSET(11),
+    LAYER_OFFSET(12),
+    LAYER_OFFSET(13),
+    LAYER_OFFSET(14),
+    LAYER_OFFSET(15),
+};
 
 static const struct layer_io layer_io[LAYER_NUM_OF_ROWS] =
 {
@@ -118,6 +183,7 @@ static const struct layer_io layer_io[LAYER_NUM_OF_ROWS] =
     LAYER_IO(9, D),
     LAYER_IO(8, D),
 };
+#endif
 
 static const struct dma_config layer_dma_config; // No special config needed
 static const struct spi_config layer_spi_config =
@@ -134,14 +200,11 @@ static const struct layer_io* layer_row_io = layer_io;
 static const struct layer_io* layer_row_previous_io = &layer_io[LAYER_NUM_OF_ROWS - 1];
 static struct dma_channel* layer_dma_channel = NULL;
 static struct spi_module* layer_spi_module = NULL;
+static struct timer_module* layer_countdown_timer = NULL;
 static enum layer_state layer_state = LAYER_IDLE;
 static enum layer_dma_state layer_dma_state = LAYER_DMA_RECEIVE_FRAME;
-static unsigned int layer_row_index = 0;
-static unsigned int layer_red_index = 0;
-static unsigned int layer_green_index = 0;
-static unsigned int layer_blue_index = 0;
-static unsigned int layer_offset = 0;
-static unsigned int layer_tlc5940_index = 0; // index of tlc5940 
+static unsigned int layer_row_index = 0; // Active row, corresponding row IO is layer_io[layer_row_index]
+static unsigned int layer_tlc5940_index = 0; // Index of tlc5940 
 static bool layer_suppress_ttask = false;
 
 bool layer_busy(void)
@@ -232,16 +295,18 @@ static void layer_ttask_execute(void)
         return;
     
     if(tlc5940_ready()) {
-        layer_offset = layer_next_row_index() * LAYER_NUM_OF_COLS;
+        unsigned int offset = layer_offset[layer_next_row_index()];
+        unsigned int r, g ,b;
+        
         for(unsigned int i = 0; i < LAYER_NUM_OF_COLS; i++) {
             // Convert 8 bit to 12 bit equivalent
-            layer_red_index = i + layer_offset + LAYER_RED_OFFSET;
-            layer_green_index = i + layer_offset + LAYER_GREEN_OFFSET;
-            layer_blue_index = i + layer_offset + LAYER_BLUE_OFFSET;
+            r = i + offset + LAYER_RED_OFFSET;
+            g = i + offset + LAYER_GREEN_OFFSET;
+            b = i + offset + LAYER_BLUE_OFFSET;
             
-            tlc5940_write_grayscale(0, i, (layer_dma_ptr[layer_red_index] << 4) | (layer_dma_ptr[layer_red_index] >> 4));
-            tlc5940_write_grayscale(1, i, (layer_dma_ptr[layer_green_index] << 4) | (layer_dma_ptr[layer_green_index] >> 4));
-            tlc5940_write_grayscale(2, i, (layer_dma_ptr[layer_blue_index] << 4) | (layer_dma_ptr[layer_blue_index] >> 4));
+            tlc5940_write_grayscale(0, i, layer_dma_ptr[r] << 4 | layer_dma_ptr[r] >> 4);
+            tlc5940_write_grayscale(1, i, layer_dma_ptr[g] << 4 | layer_dma_ptr[g] >> 4);
+            tlc5940_write_grayscale(2, i, layer_dma_ptr[b] << 4 | layer_dma_ptr[b] >> 4);
         }
         tlc5940_update();
     }
@@ -268,15 +333,20 @@ static int layer_rtask_init(void)
     REG_SET(LAYER_SCK_TRIS, LAYER_SCK_PIN_MASK);
     REG_SET(LAYER_SS_TRIS, LAYER_SS_PIN_MASK);
 
+    // Initialize TIMER
+    layer_countdown_timer = timer_construct(TIMER_TYPE_COUNTDOWN, NULL);
+    if(NULL == layer_countdown_timer)
+        goto fail_timer;
+    
     // Initialize DMA
     layer_dma_channel = dma_construct(layer_dma_config);
     if(NULL == layer_dma_channel)
-        goto deinit_dma;
+        goto fail_dma;
     
     // Initialize SPI
     layer_spi_module = spi_construct(LAYER_SPI_CHANNEL, layer_spi_config);
     if(NULL == layer_spi_module)
-        goto deinit_spi;
+        goto fail_spi;
     spi_configure_dma_src(layer_spi_module, layer_dma_channel); // SPI module is the source of the dma module
     spi_enable(layer_spi_module);
     
@@ -284,11 +354,12 @@ static int layer_rtask_init(void)
     layer_exec_lod();
    
     return KERN_INIT_SUCCCES;
-    
-deinit_spi:
-    spi_destruct(layer_spi_module);
-deinit_dma:
+
+fail_spi:
     dma_destruct(layer_dma_channel);
+fail_dma:
+    timer_destruct(layer_countdown_timer);
+fail_timer:
 
     return KERN_INIT_FAILED;
 }
@@ -341,9 +412,19 @@ static void layer_rtask_execute(void)
             if (tlc5940_ready()) {
                 // Give user some time to see the broken LED
                 bool error = layer_lod_errors[layer_tlc5940_index][layer_row_index];
+                if(error) {
+                    // Error shown, give user some time to see broken LEDs
+                    timer_start(layer_countdown_timer, LAYER_LOD_SHOW_USER_TIME, TIMER_TIME_UNIT_MS);
+                    layer_state = LAYER_EXEC_LOD_SHOW_USER_WAIT;
+                } else
+                    layer_state = LAYER_EXEC_LOD_SHOW_DONE; // No errors shown, go to next row
                 if (error) for (unsigned int i = 0; i < 1000000; i++) Nop(); // @Commit: change with timer
                 layer_state = LAYER_EXEC_LOD_SHOW_DONE;
             }
+            break;
+        case LAYER_EXEC_LOD_SHOW_USER_WAIT:
+            if(timer_timed_out(layer_countdown_timer))
+                layer_state = LAYER_EXEC_LOD_SHOW_DONE;
             break;
         case LAYER_EXEC_LOD_SHOW_DONE:
             layer_state = (layer_row_index != (LAYER_NUM_OF_ROWS - 1) || ++layer_tlc5940_index < LAYER_FRAME_DEPTH)
