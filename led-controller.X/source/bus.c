b0dbdd9be223283fe320f94044a37625dbd1bae3
LimpSquid <git@limpsquid.nl>
2021-11-11 14:25:42 +0100

    Make it so that both the response and request are the same message and a slave can distinguish them

--- led-controller.X/source/bus.c
+++ led-controller.X/source/bus.c
@@ -2,15 +2,19 @@
 /* 
  * We need no complexity at all for our bus protocol. A message might look like this:
  *
- *  master: |address byte|command byte|data byte 1|data byte 2|data byte 3|data byte 4|crc byte 1|crc byte 2|
- *  slave:  |response status byte|data byte 1|data byte 2|data byte 3|data byte 4|crc byte 1|crc byte 2|
+ *  master: |header byte|command byte |data byte 1|data byte 2|data byte 3|data byte 4|crc byte 1|crc byte 2|
+ *  slave:  |header byte|response byte|data byte 1|data byte 2|data byte 3|data byte 4|crc byte 1|crc byte 2|
  * 
- * The protocol uses a fixed message width with 1 address byte, 1 command byte, 4 data bytes and two CRC bytes. 
- * If the relevant command doesn't use any of the data bytes, zeroing these fields will suffice (not required).
+ * The protocol uses a fixed message width with 1 header byte, 1 command/response byte, 4 data bytes and two CRC bytes. 
+ * If the relevant command doesn't use any of the data bytes, zeroing these fields will suffice.
  * 
  * Because we use a fixed message width we don't need any message idle time to indicate that a tranmission
  * has completed. We can simply listen to the incoming stream of bytes and always assume the format
  * as described above.
+ * 
+ * The header describes if this frame is either a request or response. If it is a response, all slave nodes should
+ * ignore the frame. If it is a request all slave nodes should check the frame's address to see if it matches their
+ * own address or the broadcast address.
  * 
  * In case a communication error (UART error, CRC, etc) is detected on a slave node, the slave should back off for a fixed to 
  * be determined time and clear its receive buffer afterwards. Once the back off time has expired
@@ -38,38 +42,36 @@
 #include <stddef.h>
 #include <string.h>
 
-#define BUS_REQUEST_SIZE            sizeof(union bus_request)
-#define BUS_RESPONSE_SIZE           sizeof(union bus_response)
+#define BUS_FRAME_SIZE              sizeof(struct bus_frame)
 #define BUS_CRC_SIZE                sizeof(crc16_t)
 #define BUS_ERROR_BACKOFF_TIME      5 // In milliseconds
-#define BUS_BROADCAST_ADDRESS       255
+#define BUS_BROADCAST_ADDRESS       63
 
-struct __attribute__((packed)) bus_request_frame
+struct bus_header
 {
-    unsigned char address;
-    unsigned char command;
+    unsigned char request   :1; // Response if false
+    unsigned char address   :6; // Must be specified if request is true
+    unsigned char _         :1;
+};
+
+struct __attribute__((packed)) bus_frame
+{
+    struct bus_header header;
+    union
+    {
+        unsigned char command; // Request
+        unsigned char response_code; // Response
+    };
     union bus_data payload;
     crc16_t crc;
 };
 
-struct __attribute__((packed)) bus_response_frame
+union bus_raw_frame
 {
-    unsigned char response_code;
-    union bus_data payload;
-    crc16_t crc;
+    struct bus_frame frame;
+    unsigned char data[BUS_FRAME_SIZE];
 };
-
-union bus_request
-{
-    struct bus_request_frame frame;
-    unsigned char data[sizeof(struct bus_request_frame)];
-};
-
-union bus_response
-{
-    struct bus_response_frame frame;
-    unsigned char data[sizeof(struct bus_response_frame)];
-};
+STATIC_ASSERT(BUS_FRAME_SIZE == sizeof(union bus_raw_frame))
 
 enum bus_state
 {
@@ -101,8 +103,8 @@ static struct rs485_error_notifier bus_error_notifier =
 
 static struct timer_module* bus_backoff_timer = NULL;
 static crc16_t bus_crc16;
-static union bus_request bus_request;
-static union bus_response bus_response;
+static union bus_raw_frame bus_request;
+static union bus_raw_frame bus_response;
 static enum bus_state bus_state = BUS_INIT;
 static unsigned int bus_frame_offset;
 static unsigned char bus_address;
@@ -142,19 +144,19 @@ static void bus_rtask_execute(void)
         case BUS_READ_CLEAR:
             bus_frame_offset = 0;
             crc16_reset(&bus_crc16);
-            memset(bus_response.data, 0, BUS_RESPONSE_SIZE);
+            memset(bus_response.data, 0, BUS_FRAME_SIZE);
             bus_state = BUS_READ_PART;
             break;
         case BUS_READ_PART:
             if(rs485_bytes_available()) { // Shouldn't strictly be necessary
                 unsigned int size = rs485_read_buffer(
                     bus_request.data + bus_frame_offset, 
-                    BUS_REQUEST_SIZE - bus_frame_offset);
+                    BUS_FRAME_SIZE - bus_frame_offset);
                 crc16_update(&bus_crc16, bus_request.data + bus_frame_offset, size);
                 bus_frame_offset += size;
                 
                 // Did we read a whole frame's worth of data?
-                if(bus_frame_offset >= BUS_REQUEST_SIZE)
+                if(bus_frame_offset >= BUS_FRAME_SIZE)
                     bus_state = BUS_FRAME_VERIFY;
             }
             break;
@@ -167,9 +169,10 @@ static void bus_rtask_execute(void)
             if(bus_crc16)
 #endif
                 bus_state = BUS_ERROR;
-            // Frame meant for us?
-            else if(bus_request.frame.address == BUS_BROADCAST_ADDRESS ||
-                    bus_request.frame.address == bus_address)
+            // Is frame a request and meant for us?
+            else if(bus_request.frame.header.request && (
+                    bus_request.frame.header.address == BUS_BROADCAST_ADDRESS ||
+                    bus_request.frame.header.address == bus_address))
                 bus_state = BUS_FRAME_HANDLE;
             // Nope...
             else
@@ -180,7 +183,7 @@ static void bus_rtask_execute(void)
                 bus_response.frame.response_code = BUS_ERR_INVALID_COMMAND;
                 bus_state = BUS_SEND_RESPONSE;
             } else {
-                bool broadcast = bus_request.frame.address == BUS_BROADCAST_ADDRESS;
+                bool broadcast = bus_request.frame.header.address == BUS_BROADCAST_ADDRESS;
                 bus_func_t handler = bus_funcs[bus_request.frame.command];
                 ASSERT_NOT_NULL(handler);
                 
@@ -192,9 +195,11 @@ static void bus_rtask_execute(void)
             }
             break;
         case BUS_SEND_RESPONSE:
+            ASSERT(!bus_response.frame.header.request);
+            ASSERT(bus_response.frame.header.address == 0); // It reality it doesn't really matter what value it is
             crc16_reset(&bus_response.frame.crc);
-            crc16_update(&bus_response.frame.crc, bus_response.data, BUS_RESPONSE_SIZE - BUS_CRC_SIZE);
-            rs485_transmit_buffer(bus_response.data, BUS_RESPONSE_SIZE); 
+            crc16_update(&bus_response.frame.crc, bus_response.data, BUS_FRAME_SIZE - BUS_CRC_SIZE);
+            rs485_transmit_buffer(bus_response.data, BUS_FRAME_SIZE); 
             bus_state = BUS_READ_CLEAR;
             break;
             
