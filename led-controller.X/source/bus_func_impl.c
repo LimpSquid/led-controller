4b8f6ecb0396d13af38d6ce4ab62c9dd1562debe
LimpSquid <git@limpsquid.nl>
2021-11-17 00:29:48 +0100

    polish

--- led-controller.X/source/bus_func_impl.c
+++ led-controller.X/source/bus_func_impl.c
@@ -41,7 +41,7 @@ static enum bus_response_code bus_func_layer_exec_lod(
 {
     UNUSED3(broadcast, request_data, response_data);
 
-    return (layer_ready() && layer_exec_lod()) ? BUS_OK : BUS_ERR_AGAIN;
+    return layer_exec_lod() ? BUS_OK : BUS_ERR_AGAIN;
 }
 
 static enum bus_response_code bus_func_layer_dma_reset(
